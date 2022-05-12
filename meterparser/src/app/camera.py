663921e8f29f93b001e9c44274272a22157a6721
Marcos Junior <junalmeida@gmail.com>
2022-05-12 12:00:58 -0500

    Less convoluted log messages

--- meterparser/src/app/camera.py
+++ meterparser/src/app/camera.py
@@ -18,21 +18,27 @@ from app.parsers.parser_digits_gvision import parse_digits_gvision
 import math
 from skimage.metrics import structural_similarity as compare_ssim
 
+
 class Camera (threading.Thread):
     def __init__(self, camera: dict, entity_id: str, mqtt: Mqtt, debug_path: str):
         threading.Thread.__init__(self)
         self.error_limit = 30
-        self.reading_limit = 3 # 3 m3? 3 kWh? 3 what? TODO: Check if this is a good fail safe parameter.
+        # 3 m3? 3 kWh? 3 what? TODO: Check if this is a good fail safe parameter.
+        self.reading_limit = 3
         self._wait = threading.Event()
         self._interval = int(camera["interval"])
         self._snapshot_url = str(camera["snapshot_url"])
         self.name = str(camera["name"])
-        self._device_class = str(camera["device_class"]) if "device_class" in camera else "energy"
-        self._unit_of_measurement = str(camera["unit_of_measurement"]) if "unit_of_measurement" in camera else "kWh"
+        self._device_class = str(
+            camera["device_class"]) if "device_class" in camera else "energy"
+        self._unit_of_measurement = str(
+            camera["unit_of_measurement"]) if "unit_of_measurement" in camera else "kWh"
         self._disposed = False
-        self._current_reading = float(data[entity_id]) if entity_id in data else 0.0
+        self._current_reading = float(
+            data[entity_id]) if entity_id in data else 0.0
         self._dials = camera["dials"] if "dials" in camera else []
-        self._dial_size = int(camera["dial_size"]) if "dial_size" in camera else 100
+        self._dial_size = int(
+            camera["dial_size"]) if "dial_size" in camera else 100
         self._digits = int(camera["digits"]) if "digits" in camera else 0
         self._decimals = int(camera["decimals"]) if "decimals" in camera else 0
         self._ocr_space_key = camera["ocr_space_key"] if "ocr_space_key" in camera else None
@@ -44,22 +50,27 @@ class Camera (threading.Thread):
         self._debug_path = debug_path
         self._previous_image = None
         if self._digits == 0 and len(self._dials) == 0:
-            raise Exception("Incorrect setup. Set this camera to use either digits or dials.")
-        if self._interval < 30: 
-            raise Exception("Incorrect interval in seconds. Choose more than 30 seconds.")
-        
+            raise Exception(
+                "Incorrect setup. Set this camera to use either digits or dials.")
+        if self._interval < 30:
+            raise Exception(
+                "Incorrect interval in seconds. Choose more than 30 seconds.")
+
     def stop(self):
         self._wait.set()
         self._logger.warn("Stopping %s camera..." % self.entity_id)
 
     def run(self):
         self._logger.info("Starting camera %s" % self.name)
-        topic_listen = self._mqtt.mqtt_subscribe("sensor", self.entity_id, self.set_callback)
+        topic_listen = self._mqtt.mqtt_subscribe(
+            "sensor", self.entity_id, self.set_callback)
+        self._logger.info("Listening to messages at topic %s" % (topic_listen))
         while not self._wait.is_set():
             try:
-                self._mqtt.mqtt_sensor_discovery(self.entity_id, self.name, self._device_class, self._unit_of_measurement)
-                self._mqtt.mqtt_set_availability("sensor", self.entity_id, self._error_count < self.error_limit)      
-                self._logger.info("Listening to messages at topic %s" % (topic_listen))
+                self._mqtt.mqtt_sensor_discovery(
+                    self.entity_id, self.name, self._device_class, self._unit_of_measurement)
+                self._mqtt.mqtt_set_availability(
+                    "sensor", self.entity_id, self._error_count < self.error_limit)
 
                 reading = 0.0
                 img = None
@@ -69,14 +80,16 @@ class Camera (threading.Thread):
                         img = self.get_image()
                         self.send_image(img)
 
-                        img = prepare_image(img, self.entity_id, self.send_image, self._debug_path)
+                        img = prepare_image(
+                            img, self.entity_id, self.send_image, self._debug_path)
                         self.send_image(img)
-                        
+
                     except Exception as e:
                         img = None
                         image_error += 1
                         if image_error > 5:
-                            self._mqtt.mqtt_set_availability("camera", self.entity_id, False) 
+                            self._mqtt.mqtt_set_availability(
+                                "camera", self.entity_id, False)
                             raise e
                         self._wait.wait(10)
 
@@ -85,14 +98,17 @@ class Camera (threading.Thread):
                     (imgH, imgW, _) = img.shape
                     (pimgH, pimgW, _) = self._previous_image.shape
                     if imgH != pimgH or imgW != pimgW:
-                        self._previous_image = cv2.resize(self._previous_image, (imgW, imgH))
-                    grayA = cv2.cvtColor(self._previous_image, cv2.COLOR_BGR2GRAY)
+                        self._previous_image = cv2.resize(
+                            self._previous_image, (imgW, imgH))
+                    grayA = cv2.cvtColor(
+                        self._previous_image, cv2.COLOR_BGR2GRAY)
                     grayB = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                     (score, _) = compare_ssim(grayA, grayB, full=True)
                     # diff = (diff * 255).astype("uint8")
                     if score > 0.93:
                         should_process = False
-                        self._logger.debug("Previous and current images have %d percent of simmilarity, so not wasting an OCR call." % score)
+                        self._logger.debug(
+                            "Previous and current images have %d percent of simmilarity, so not wasting an OCR call." % score)
 
                 if should_process:
                     self._previous_image = img.copy()
@@ -125,60 +141,71 @@ class Camera (threading.Thread):
                             debug_path=self._debug_path,
                         )
                     else:
-                        raise Exception("Invalid configuration. Set either dials or digits to scan. For digits set an API key.")
-                    upper_limit = self._current_reading + self.reading_limit 
-                    lower_limit = self._current_reading - self.reading_limit 
+                        raise Exception(
+                            "Invalid configuration. Set either dials or digits to scan. For digits set an API key.")
+                    upper_limit = self._current_reading + self.reading_limit
+                    lower_limit = self._current_reading - self.reading_limit
                     if reading > 0 and reading >= self._current_reading and (self._current_reading == 0 or reading < upper_limit):
 
-                        self._logger.info("Got a valid reading\ncurrent=%s, previous=%s, limit=%s" % (reading, self._current_reading, upper_limit))
+                        self._logger.info("Valid reading - current=%s, previous=%s, limit=%s" % (
+                            reading, self._current_reading, upper_limit))
                         self._current_reading = reading
                         data[self.entity_id] = self._current_reading
-                        
+
                         self._error_count = 0
                     elif reading > 0 and reading >= lower_limit:
-                        # if reading is close to last reading, let's send again last reading so
+                        # if reading is close to last reading (see self.reading_limit), let's send again last reading so
                         # it won't be available by a small amount of difference.
+                        self._logger.info("Close reading - current=%s, previous=%s, limit=%s, not updating." % (
+                            reading, self._current_reading, upper_limit))
                         self._error_count = 0
                     else:
-                        self._logger.error("Got an invalid reading. Value could be too high or less than previous.\ncurrent=%s, previous=%s, limit=%s" % (reading, self._current_reading, upper_limit))
-                        self._error_count += 1               
+                        self._logger.error("Invalid reading - current=%s, previous=%s, limit=%s - Value could be too high or less than previous reading." % (
+                            reading, self._current_reading, upper_limit))
+                        self._error_count += 1
 
             except Exception as e:
                 err = {
-                        "last_error": "%s" % e,
-                        "last_error_at": time.strftime("%Y-%m-%d %H:%M:%S")
-                    }
+                    "last_error": "%s" % e,
+                    "last_error_at": time.strftime("%Y-%m-%d %H:%M:%S")
+                }
                 self._logger.error(err["last_error"])
                 self._mqtt.mqtt_set_attributes("sensor", self.entity_id, err)
                 self._error_count += 1
 
-            self._mqtt.mqtt_set_availability("sensor", self.entity_id, self._error_count < self.error_limit)      
+            self._mqtt.mqtt_set_availability(
+                "sensor", self.entity_id, self._error_count < self.error_limit)
             if self._error_count < self.error_limit and self._current_reading > 0:
                 # send to mqtt
-                self._mqtt.mqtt_set_state("sensor", self.entity_id, self._current_reading)
+                self._mqtt.mqtt_set_state(
+                    "sensor", self.entity_id, self._current_reading)
                 err = {
                     "last_error": None,
                     "last_error_at": None
                 }
                 self._mqtt.mqtt_set_attributes("sensor", self.entity_id, err)
-            self._logger.debug("Waiting %s secs for next reading." % self._interval)              
+            self._logger.debug(
+                "Waiting %s secs for next reading." % self._interval)
             self._wait.wait(self._interval)
         self._logger.warn("Camera %s is now disposed." % self.name)
         self._disposed = True
 
     def get_image(self):
-        req = requests.get(self._snapshot_url, stream=True) # TODO: get image from video stream
+        # TODO: get image from video stream
+        req = requests.get(self._snapshot_url, stream=True)
         if req.status_code == 200:
             stream = req.raw
             arr = np.asarray(bytearray(stream.read()), dtype=np.uint8)
-            img = cv2.imdecode(arr, cv2.IMREAD_UNCHANGED) # 'Load it as it is'
+            img = cv2.imdecode(arr, cv2.IMREAD_UNCHANGED)  # 'Load it as it is'
             return img
         else:
             raise Exception(req.text)
+
     def send_image(self, image):
-        ret_code, jpg_buffer = cv2.imencode(".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])     
-        
-        self._mqtt.mqtt_set_availability("camera", self.entity_id, True) 
+        ret_code, jpg_buffer = cv2.imencode(
+            ".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+        self._mqtt.mqtt_set_availability("camera", self.entity_id, True)
         self._mqtt.mqtt_set_state("camera", self.entity_id, bytes(jpg_buffer))
 
     def set_callback(self, client, userdata, message):
@@ -186,7 +213,8 @@ class Camera (threading.Thread):
             message = json.loads(message.payload.decode("utf-8"))
             if message is not None and "value" in message and str(message["value"]).isnumeric():
                 self._current_reading = float(message["value"])
-                self._logger.info("Resetting %s (%s) to %d" % (self.name, self.entity_id, self._current_reading))
+                self._logger.info("Resetting %s (%s) to %d" % (
+                    self.name, self.entity_id, self._current_reading))
                 data[self.entity_id] = self._current_reading
             else:
                 self._logger.error("Invalid payload %s" % message)
